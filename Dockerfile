00783b46e5ca89985f65fbd0f99c8ad8b444e0ab
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-06-03 10:55:07 +0000

    Updated template

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3.8.1
 
+WORKDIR /usr/src/app
+
 COPY . .
 
-RUN python -m limp install_deps
+RUN python . install_deps
 
 EXPOSE 8081
 
-CMD [ "python", "-m", "limp", "launch" ]
+CMD [ "python", ".", "launch" ]
--- packages/PROJECT_NAME/__init__.py
+++ packages/PROJECT_NAME/__init__.py
@@ -4,9 +4,9 @@ from nawah.classes import PACKAGE_CONFIG
 
 
 config = PACKAGE_CONFIG(
-	api_level='6.1',
+	api_level='0.1',
 	# Use sematic versioning for package version, so you keep package version and major corresponding to API level, while minor is increamental per new version of the package, and reset it for new API level
-	version='6.1.0',
+	version='0.1.0',
 	# Define package vars. This is shared storage across packages so you can have multiple packages communicating per specific instructions defined here. You can also use this to define 'globals' dict which you can use across your modules
 	# vars={
 	# 	'globals':{
