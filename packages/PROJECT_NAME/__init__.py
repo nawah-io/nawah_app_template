a36526e7f7536696417c85a8c975956d562d6cd9
Mahmoud Abduljawad <mahmoud@masaar.com>
2021-06-16 17:24:55 +0000

    Update default package Config Attr api_level to 1.1

--- packages/PROJECT_NAME/__init__.py
+++ packages/PROJECT_NAME/__init__.py
@@ -4,7 +4,7 @@ from nawah.classes import PACKAGE_CONFIG
 
 
 config = PACKAGE_CONFIG(
-    api_level='1.0',
+    api_level='1.1',
     # Use sematic versioning for package version, so you keep package version and major corresponding to API level, while minor is increamental per new version of the package, and reset it for new API level
     version='1.0.0',
     # Define package vars. This is shared storage across packages so you can have multiple packages communicating per specific instructions defined here. You can also use this to define 'globals' dict which you can use across your modules
